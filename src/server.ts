a5cc5c9855525cc77e06a8fde54e4a69e927cef2
kingsky32 <kingsky32@gmail.com>
2021-06-04 22:24:52 +0900

    Fix Type error in Node Modules

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import { GraphQLServer } from "graphql-yoga";
 import cors from "cors";
+import { GraphQLServer } from "graphql-yoga";
 import helmet from "helmet";
 import logger from "morgan";
 import schema from "./schema";
 
-const server = new GraphQLServer({ schema });
+const server: GraphQLServer = new GraphQLServer({ schema });
 
 server.express.use(cors());
 server.express.use(helmet());
