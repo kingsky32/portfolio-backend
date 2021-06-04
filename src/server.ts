c3bf2c318efc310e1ec85e05b249936af551cb46
kingsky32 <kingsky32@gmail.com>
2021-06-04 22:39:57 +0900

    fix loading bug

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,9 @@ import schema from "./schema";
 const server: GraphQLServer = new GraphQLServer({ schema });
 
 server.express.use(cors());
-server.express.use(helmet());
+server.express.use(
+  helmet({ contentSecurityPolicy: process.env.NODE_ENV === "production" ? undefined : false })
+);
 server.express.use(logger("dev"));
 
 export default server;
