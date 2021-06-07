b92d4bf3514817693f561574a1dcc5d2a766faf9
kingsky32 <kingsky32@gmail.com>
2021-06-07 20:29:31 +0900

    Getting Graphql merge

---
+++ src/types/graph.d.ts
@@ -0,0 +1,64 @@
+export const typeDefs = ["type Mutation {\n  CreateProject(type: String, platform: String, title: String, thumbnail: Int, github: String, page: String, tools: [String], startAt: Date, endAt: Date): Project\n  DeleteProject(id: Int!): Boolean\n  UpdateProject(id: Int!, type: String, platform: String, title: String, thumbnail: Int, github: String, page: String, tools: [String], startAt: Date, endAt: Date): Boolean\n}\n\ntype Query {\n  GetProject(id: Int!): Project\n  GetProjects: [Project]\n}\n\nscalar Date\n\ntype Project {\n  id: Int\n  accountId: Int\n  type: String\n  platform: String\n  title: String\n  thumbnail: Int\n  github: String\n  page: String\n  tools: [String]\n  startAt: Date\n  endAt: Date\n  createdAt: Date\n  updatedAt: Date\n}\n"];
+/* tslint:disable */
+
+export interface Query {
+  GetProject: Project | null;
+  GetProjects: Array<Project> | null;
+}
+
+export interface GetProjectQueryArgs {
+  id: number;
+}
+
+export interface Project {
+  id: number | null;
+  accountId: number | null;
+  type: string | null;
+  platform: string | null;
+  title: string | null;
+  thumbnail: number | null;
+  github: string | null;
+  page: string | null;
+  tools: Array<string> | null;
+  startAt: Date | null;
+  endAt: Date | null;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+}
+
+export type Date = any;
+
+export interface Mutation {
+  CreateProject: Project | null;
+  DeleteProject: boolean | null;
+  UpdateProject: boolean | null;
+}
+
+export interface CreateProjectMutationArgs {
+  type: string | null;
+  platform: string | null;
+  title: string | null;
+  thumbnail: number | null;
+  github: string | null;
+  page: string | null;
+  tools: Array<string> | null;
+  startAt: Date | null;
+  endAt: Date | null;
+}
+
+export interface DeleteProjectMutationArgs {
+  id: number;
+}
+
+export interface UpdateProjectMutationArgs {
+  id: number;
+  type: string | null;
+  platform: string | null;
+  title: string | null;
+  thumbnail: number | null;
+  github: string | null;
+  page: string | null;
+  tools: Array<string> | null;
+  startAt: Date | null;
+  endAt: Date | null;
+}
