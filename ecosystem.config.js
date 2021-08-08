e8ca3ee9556a766b8cb6cd7a99fbd137ef05a48f
Seung Ju <kingsky32@gmail.com>
2021-08-08 23:52:05 +0900

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'portfolio-server',
-      script: './dist/index.js', 
+      script: 'npm',
+      args: 'start',
       watch: false,
       ignore_watch: ['[/\\]./', 'node_modules', 'logs'],
       exec_mode: 'cluster',
     },
   ],
 };
-
