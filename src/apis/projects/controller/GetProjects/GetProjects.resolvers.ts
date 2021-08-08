d09eac660d15e261b8c58d1076f1f1a401ef7b91
Seung Ju <kingsky32@gmail.com>
2021-08-08 23:13:04 +0900

    Update GetProjects.resolvers.ts

--- src/apis/projects/controller/GetProjects/GetProjects.resolvers.ts
+++ src/apis/projects/controller/GetProjects/GetProjects.resolvers.ts
@@ -6,7 +6,6 @@ export default {
     GetProjects: async (): Promise<ProjectProps[]> => {
       try {
         const data = await Projects.findAll();
-        console.log(data);
         return data;
       } catch (error) {
         throw error;
